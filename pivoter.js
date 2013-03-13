c5802976f2163098a4f2d09953a12a4086a44054
Carl Grundberg <carl.grundberg@menmo.se>
2013-03-13 19:14:06 +0100

    changed keys to index value

--- pivoter.js
+++ pivoter.js
@@ -1,19 +1,19 @@
 Votes = new Meteor.Collection("votes");
 
-// should be somply array, but cant figure out how to use 'this' in selectedItem
+// should be array, but cant figure out how to use 'this' in selectedItem
 GridItems = {
     0:0,
     1:1,
     2:2,
     3:3,
-    5:5,
-    8:8,
-    13:13,
-    21:21,
-    34:34,
-    55:55,
-    '?':'?',
-    '<img src="cup.png" />':'<img src="cup.png" />' };
+    4:5,
+    5:8,
+    6:13,
+    7:21,
+    8:34,
+    9:55,
+    10:'?',
+    11:'<img src="cup.png" />' };
 
 if (Meteor.isClient) {
     Template.status.votes = function () {
